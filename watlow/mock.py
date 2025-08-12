b6ab8ce3ae197cbef16ffd2c6afc60fad1c19c6e
Alex Ruddick <alex@ruddick.tech>
2025-08-12 12:26:33 -0500

    eliminate self.state

--- watlow/mock.py
+++ watlow/mock.py
@@ -39,7 +39,6 @@ class Gateway(realGateway):
 
     def __init__(self, *args, max_temp=220, **kwargs):
         self.setpoint_range = (10, max_temp)
-        self.state = {i: {'actual': 25.0, 'setpoint': 25.0, 'output': 0.0} for i in range(1, 9)}
         self.client = AsyncClientMock()
         self._detect_pymodbus_version()
         self.actual_temp_address = 360
@@ -47,59 +46,62 @@ class Gateway(realGateway):
         self.output_address = 1904
         self.modbus_offset = 5000
         self._registers: dict[int, int] = {}
-        self._update_registers_from_state()
 
-    @property
-    def base_addresses(self):
-        """Return address constants."""
-        return {
-            'actual': self.actual_temp_address,
-            'setpoint': self.setpoint_address,
-            'output': self.output_address,
-        }
+        # Initialize _registers with default 25.0 for actual/setpoint, 0.0 output
+        for zone in range(1, 9):
+            zone_offset = self.modbus_offset * (zone - 1)
+            for addr, val in [
+                (self.actual_temp_address + zone_offset, 25.0),
+                (self.setpoint_address + zone_offset, 25.0),
+                (self.output_address + zone_offset, 0.0),
+            ]:
+                hi, lo = struct.unpack('>HH', struct.pack('>f', val))
+                self._registers[addr] = hi
+                self._registers[addr + 1] = lo
+
+    def _read_float(self, addr: int) -> float:
+        hi = self._registers.get(addr, 0)
+        lo = self._registers.get(addr + 1, 0)
+        packed = struct.pack('>HH', hi, lo)
+        return struct.unpack('>f', packed)[0]
+
+    def _write_float(self, addr: int, val: float):
+        hi, lo = struct.unpack('>HH', struct.pack('>f', val))
+        self._registers[addr] = hi
+        self._registers[addr + 1] = lo
 
     def _perturb(self):
-        for temps in self.state.values():
-            if temps['actual'] < temps['setpoint']:
-                temps['actual'] += 1
-                temps['output'] = min(temps['setpoint'] - temps['actual'], 100)
-            elif temps['actual'] > temps['setpoint']:
-                temps['actual'] -= 1
-                temps['output'] = 0
-        self._update_registers_from_state()
-
-    def _update_registers_from_state(self):
-        for zone, temps in self.state.items():
+        for zone in range(1, 9):
             zone_offset = self.modbus_offset * (zone - 1)
-            for param, base_addr in self.base_addresses.items():
-                val = temps[param]
-                hi, lo = struct.unpack('>HH', struct.pack('>f', val))
-                reg = base_addr + zone_offset
-                self._registers[reg] = hi
-                self._registers[reg + 1] = lo
+            actual_addr = self.actual_temp_address + zone_offset
+            setpoint_addr = self.setpoint_address + zone_offset
+            output_addr = self.output_address + zone_offset
 
-    def _update_state_from_registers(self):
-        for zone in self.state:
-            zone_offset = self.modbus_offset * (zone - 1)
-            for param, base_addr in self.base_addresses.items():
-                reg = base_addr + zone_offset
-                hi = self._registers.get(reg, 0)
-                lo = self._registers.get(reg + 1, 0)
-                packed = struct.pack('>HH', hi, lo)
-                val = struct.unpack('>f', packed)[0]
-                self.state[zone][param] = val
+            actual = self._read_float(actual_addr)
+            setpoint = self._read_float(setpoint_addr)
+            output = self._read_float(output_addr)
+
+            if actual < setpoint:
+                actual += 1
+                output = min(setpoint - actual, 100)
+            elif actual > setpoint:
+                actual -= 1
+                output = 0
+
+            self._write_float(actual_addr, actual)
+            self._write_float(setpoint_addr, setpoint)
+            self._write_float(output_addr, output)
 
     async def _request(self, method, address, count, **kwargs):
         if method == 'read_holding_registers':
             regs = [self._registers.get(address + i, 0) for i in range(count)]
             if pymodbus38plus:
-                return ReadHoldingRegistersResponse(registers=regs)  # type: ignore
-            return ReadHoldingRegistersResponse(regs)
+                return ReadHoldingRegistersResponse(registers=regs)
+            return ReadHoldingRegistersResponse(regs)  # type: ignore
 
         if method == 'write_registers':
             for i, val in enumerate(count):
                 self._registers[address + i] = val
-            self._update_state_from_registers()
             self._perturb()
             return WriteMultipleRegistersResponse(address, count)
 
